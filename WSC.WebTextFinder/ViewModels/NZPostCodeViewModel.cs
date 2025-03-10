5ac4aaf91a667ebb203303b86069eb88a01e3e48
Jeff Buot <jeffbuot14@gmail.com>
2025-03-10 14:41:12 +0800

    Updated branding

--- WSC.WebTextFinder/ViewModels/NZPostCodeViewModel.cs
+++ WSC.WebTextFinder/ViewModels/NZPostCodeViewModel.cs
@@ -8,4 +8,14 @@ using WSC.WebTextFinder.Core;
 namespace WSC.WebTextFinder.ViewModels {
     public class NZPostCodeViewModel : ViewModelBase {
     }
+    public class NZRegion {
+        public string Name { get; set; }
+        public string UrbanPostCodes { get; set; }
+        public string RuralPostCodes { get; set; }
+    }
+    public enum Island {
+        North,
+        South,
+        Other
+    }
 }
